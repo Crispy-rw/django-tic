d2ad77fc58cbd5e8a301a4bb5baff2bd51bba4d4
elemanhillary <elemanhillary@gmail.com>
2020-04-23 01:27:55 +0200

    Finished game

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -21,5 +21,6 @@ from .views import welcome
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^player/', include('player.urls')),
+    url(r'^games/', include('gameplay.urls')),
     url(r'^$', welcome, name="tictactoe_welcome")
 ]
